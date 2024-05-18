df8d3b4808ad37380414a5cf5abc40c54fdb2af3
percyfikri <mafikri27@gmail.com>
2024-05-18 11:42:45 +0700

    delete dist/css/main.css

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "./dist/css/main.css";
 import { BrowserRouter } from "react-router-dom";
 
 
