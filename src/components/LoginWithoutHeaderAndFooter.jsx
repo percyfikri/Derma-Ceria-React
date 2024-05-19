07239f944ec3405430d61db7c54035d25d3ce032
percyfikri <mafikri27@gmail.com>
2024-05-20 00:04:08 +0700

    add : LoginWithoutHeaderAndFooter

---
+++ src/components/LoginWithoutHeaderAndFooter.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import Login from "./pages/loginPage/login";
+
+// const LoginWithoutHeaderAndFooter = () => {
+//   return <Login />;
+// };
+
+// export default LoginWithoutHeaderAndFooter;
+import React from 'react'
+import Login from '../pages/loginPage/login'
+
+const LoginWithoutHeaderAndFooter = () => {
+  return (
+    <Login/>
+  )
+}
+
+export default LoginWithoutHeaderAndFooter
